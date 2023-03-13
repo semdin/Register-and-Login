091ffc05641dacd29c0234f848b0ebaa69c02fe0
semdin <mehmetsemdinaktay@gmail.com>
2023-03-13 21:42:14 +0300

    database encryption and adding dotenv

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -11,8 +12,7 @@ const userSchema= new mongoose.Schema({
     password: String
 });
 
-const secret = "testSecretLetsSeeWhatItIs.";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 
 const User = new mongoose.model("User", userSchema);
