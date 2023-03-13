561e39e32dfa351c4bc77c02e9737f1e768d6813
semdin <mehmetsemdinaktay@gmail.com>
2023-03-13 22:24:36 +0300

    Adding hashing password

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+
+console.log(md5("hello"));
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
@@ -12,7 +15,7 @@ const userSchema= new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+//userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -45,7 +48,7 @@ app.get("/:direction", (req, res)=> {
 
 app.post("/register", (req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     const newUser = new User(
         {
@@ -62,7 +65,7 @@ app.post("/register", (req,res)=>{
 
 app.post("/login", (req,res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email : username}).then(function(user, err) {
         if (err) return console.error(err);
